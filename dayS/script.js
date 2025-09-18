1cf1f8d997d9b1ac33790b3296311a263e58ec23
Phennylalanine <petergregoryj@gmail.com>
2025-09-19 08:50:11 +0900

    Update script.js

--- dayS/script.js
+++ dayS/script.js
@@ -236,13 +236,13 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   function saveProgress() {
-    localStorage.setItem("EventSxp", xp);
-    localStorage.setItem("EventSlevel", level);
+    localStorage.setItem("daySxp", xp);
+    localStorage.setItem("daySlevel", level);
   }
 
   function loadProgress() {
-    const savedXP = localStorage.getItem("EventSxp");
-    const savedLevel = localStorage.getItem("EventSlevel");
+    const savedXP = localStorage.getItem("daySxp");
+    const savedLevel = localStorage.getItem("daySlevel");
 
     if (savedXP !== null) xp = parseInt(savedXP, 10);
     if (savedLevel !== null) level = parseInt(savedLevel, 10);
@@ -311,3 +311,4 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 });
+
